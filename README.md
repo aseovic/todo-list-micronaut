2a47b80d2a09fa968b07a2b3d9b48570302f90cf
Aleks Seovic <aleks@seovic.com>
2022-08-06 00:08:18 -0400

    Final working implementation

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# todo-list-micronaut
-Course materials for To Do List Micronaut workshop
+# Course materials for Coherence To Do List (Micronaut) workshop
+
+>This is a complete, final implementation of the application created during the workshop.
+> 
+>To switch to the starting point for the workshop, check out the `initial` branch:
+> 
+>```bash
+> git checkout initial
+> ```
+
+## Build Instructions
+
+### Maven
+
+```bash
+mvn clean package
+```
+
+### Gradle
+
+```bash
+./gradlew clean build
+```
+
+## Running the Example
+
+### Maven
+
+```bash  
+mvn exec:exec
+```
+
+### Gradle
+
+```bash
+./gradlew run
+```
+
+## Building a Docker Image
+
+### Maven
+
+```bash
+mvn clean install
+mvn package -P docker 
+```
+
+### Gradle
+
+```bash
+./gradlew clean jibDockerBuild
+```
+
+### Running the Docker Container
+
+```bash
+docker run -d -p 5001:5001 -P 5002:5002 ghcr.io/coherence-community/todo-list-micronaut-server
+```
+
+> NOTE: `5001` is the HTTP port, and `5002` is the metrics port.
+
+### Access the Web UI
+
+Access via http://localhost:5001/
+
+![To Do List - React Client](assets/react-client.png)
+
+### Query the GraphQL Endpoint
+
+The GraphQL Endpoint is available at http://localhost:5001/graphql. Use one of the following tools to interact wih it:
+
+- [GraphiQL](https://github.com/graphql/graphiql)
+- [Insomnia](https://insomnia.rest/download)
+
+To retrieve a collection of tasks, use the following query:
+
+```graphql
+query {
+  tasks(completed: false) {
+    id
+    description
+    completed
+    createdAt
+    createdAtDate
+  }
+}
+```
+
+## References
+
+* [Coherence CE](https://coherence.community/)
+* [Micronaut](https://micronaut.io/)
---
+++ src/main/java/com/oracle/coherence/examples/todo/server/rest/ToDoController.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
+package com.oracle.coherence.examples.todo.server.rest;
+
+import com.oracle.coherence.examples.todo.server.Task;
+import com.oracle.coherence.examples.todo.server.TaskRepository;
+import com.tangosol.net.Cluster;
+import com.tangosol.net.Member;
+
+import com.tangosol.util.Filter;
+import com.tangosol.util.Filters;
+import io.micronaut.core.annotation.Nullable;
+
+import io.micronaut.http.MediaType;
+
+import io.micronaut.http.annotation.Consumes;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.QueryValue;
+
+import io.micronaut.http.sse.Event;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.reactivestreams.Publisher;
+
+/**
+ * REST API for To Do list management.
+ */
+@Controller("/api/tasks")
+@Singleton
+public class ToDoController
+    {
+    @Inject
+    private TaskRepository tasks;
+
+    @Inject
+    private Cluster cluster;
+
+    private Flowable<Event<Task>> flowable;
+
+    @PostConstruct
+    void createBroadcaster()
+        {
+        Observable<Event<Task>> source = Observable.<Event<Task>>create(emitter ->
+            {
+            TaskRepository.Listener<Task> listener = tasks.listener()
+                    .onInsert(task -> emitter.onNext(createEvent("insert", task)))
+                    .onUpdate(task -> emitter.onNext(createEvent("update", task)))
+                    .onRemove(task -> emitter.onNext(createEvent("delete", task))).build();
+            tasks.addListener(listener);
+            emitter.setCancellable(() -> tasks.removeListener(listener));
+            }).share();
+        this.flowable = source.toFlowable(BackpressureStrategy.BUFFER);
+        }
+
+    private Event<Task> createEvent(String name, Task task)
+        {
+        return Event.of(task).name(name);
+        }
+
+    @SuppressWarnings("unchecked")
+    @Get(value = "/events", produces = MediaType.TEXT_EVENT_STREAM)
+    public Publisher<Event<?>> registerEventListener()
+        {
+        Member        member       = cluster.getLocalMember();
+        Event<String> initialEvent = Event.of(member.toString());
+        initialEvent.name("begin");
+
+        return Flowable.concatArray(Flowable.fromArray(initialEvent), flowable);
+        }
+
+    @Get(produces = MediaType.APPLICATION_JSON)
+    public Collection<Task> getTasks(@Nullable @QueryValue(value = "completed") Boolean completed)
+        {
+        Filter<Task> filter = completed == null
+                              ? Filters.always()
+                              : Filters.equal(Task::getCompleted, completed);
+
+        return tasks.getAllOrderedBy(filter, Task::getCreatedAt);
+        }
+
+    @Post(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    public Task createTask(Task task)
+        {
+        return tasks.save(new Task(task.getDescription()));
+        }
+
+    @Delete("{id}")
+    public void deleteTask(@PathVariable("id") String id)
+        {
+        tasks.removeById(id);
+        }
+
+    @Delete
+    public void deleteCompletedTasks()
+        {
+        tasks.deleteByCompletedTrue();
+        }
+
+    @Put(value = "{id}", consumes = MediaType.APPLICATION_JSON)
+    public Task updateTask(@PathVariable("id") String id, Task task)
+        {
+        String  description = task.getDescription();
+        Boolean completed   = task.getCompleted();
+
+        if (description != null)
+            {
+            return tasks.update(id, Task::setDescription, description);
+            }
+        else if (completed != null)
+            {
+            return tasks.update(id, Task::setCompleted, completed);
+            }
+
+        throw new IllegalArgumentException("either description or completion status must be specified");
+        }
+    }
